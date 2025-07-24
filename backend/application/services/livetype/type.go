c2a9821d9dbfdb604dd9cdfbb71ede64f0674280
its.arnav.sh <its.arnav.sh@gmail.com>
2025-07-24 20:08:42 +0530

    gyt

--- backend/application/services/livetype/type.go
+++ backend/application/services/livetype/type.go
@@ -9,14 +9,18 @@ import (
 )
 
 type Typer struct {
-	info   entity.TypeInfo
-	query  *database.Queries
-	logger zap.Logger
+	info    entity.TypeInfo
+	query   *database.Queries
+	logger  zap.Logger
+	keyChan chan string
 }
 
-func (t *Typer) StartTyping(ctx context.Context, Info entity.TypeInfo, query *database.Queries) {
+func (t *Typer) StartTyping(ctx context.Context, Info entity.TypeInfo, query *database.Queries) error {
 	snippet, err := query.GetRandomSnippetByLanguage(ctx, Info.Lang.String())
 	if err != nil {
-
+		t.logger.Error("Could not load snippet")
+		return err
 	}
+
+	return nil
 }
