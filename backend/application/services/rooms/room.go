95a8c3426f1d29708e4e33581321a7cfb65fdd0e
its.arnav.sh <its.arnav.sh@gmail.com>
2025-08-15 01:34:42 +0530

    New Room Handler

--- backend/application/services/rooms/room.go
+++ backend/application/services/rooms/room.go
@@ -2,9 +2,26 @@ package room
 
 import (
 	"osdtype/application/entity"
+	"osdtype/database"
+
+	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
 )
 
 type RoomHandler struct {
 	essentials entity.Essentials
-	actions    map[string]func(info string) error
+	actions    map[string]func(*gin.Context, []byte) error
+}
+
+func NewRoomHandler(db *database.Queries, logger *zap.Logger) RoomHandler {
+	rh := RoomHandler{}
+	var actions = map[string]func(*gin.Context, []byte) error{
+		"create_room":        rh.create_room,
+		"add_player":         rh.add_player,
+		"change_player_perm": rh.change_player_perms,
+		"remove_player":      rh.remove_player,
+	}
+	rh.actions = actions
+	rh.essentials = entity.Essentials{Db: db, Logger: logger}
+	return rh
 }
