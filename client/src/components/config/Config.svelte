6646f8d6b91e012ed49364ca1eb34697e404b2da
its.arnav.sh <its.arnav.sh@gmail.com>
2025-08-17 02:55:42 +0530

    Selection

--- client/src/components/config/Config.svelte
+++ client/src/components/config/Config.svelte
@@ -1,9 +1,16 @@
 <script>
 	import LanguageSel from '../typing/LanguageSel.svelte';
 	import Timer from '../typing/Timer.svelte';
+	import TimeSelector from '../typing/TimeSelector.svelte';
+	let start_timer = false;
 </script>
 
 <div class="flex flex-1 flex-row justify-between">
-	<Timer time={45} />
+	{#if start_timer}
+		<Timer time={45} />
+	{:else}
+		<TimeSelector time={45} />
+	{/if}
+
 	<LanguageSel />
 </div>
--- client/src/components/typing/Timer.svelte
+++ client/src/components/typing/Timer.svelte
@@ -8,16 +8,13 @@
 			if (remaining_time <= 0) {
 				clearInterval(interval_id);
 			}
-			if (remaining_time <= 0) {
-				clearInterval(interval_id);
-			}
 		}, 1000);
 	});
 </script>
 
 <div class="mt-10 ml-10 flex flex-col items-start justify-end space-y-1 font-mono text-white">
 	<!-- Big number -->
-	<div class="text-5xl leading-none font-bold opacity-40" style="min-width: 10ch;">
+	<div class="flex flex-row text-5xl leading-none font-bold opacity-40" style="min-width: 10ch;">
 		{remaining_time}
 	</div>
 	<!-- Label -->
