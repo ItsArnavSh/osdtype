52b3a0a14c6648497c41745e07632d9c94eae2df
its.arnav.sh <its.arnav.sh@gmail.com>
2025-08-17 14:20:29 +0530

    New FE

--- frontend/src/components/Core/header.svelte
+++
@@ -1,1 +0,0 @@
-<p class="p-4 text-center font-mono text-6xl font-bold text-white">OSDType</p>
--- frontend/src/routes/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-	import Header from '../components/Core/header.svelte';
-	import Typer from '../components/Core/TypingArea/typer.svelte';
-</script>
-
-<div class="h-lvh w-lvw bg-gray-700">
-	<Header />
-	<Typer />
-</div>
