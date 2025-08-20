b4e39a9869ccae3adbb6752351ebb8b340557493
its.arnav.sh <its.arnav.sh@gmail.com>
2025-08-20 19:58:37 +0530

    Refactor

--- backend/main.go
+++ backend/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"osdtype/application/api"
 	"osdtype/application/entity"
 	"osdtype/database"
@@ -13,19 +14,22 @@ import (
 
 func main() {
 	ctx := context.Background()
-	log, _ := zap.NewProduction()
+	log, err := zap.NewProduction()
+	defer log.Sync()
+	if err != nil {
+		fmt.Print("Could not set up logger")
+		return
+	}
 	conn, err := pgx.Connect(ctx, "postgres://user:123@localhost:5432/typedata?sslmode=disable")
 	if err != nil {
 		log.Error("Could not connect to the database")
 		return
 	}
-	defer conn.Close(ctx)
-	log.Sync()
 	query := database.New(conn)
 
 	essen := entity.Essentials{Db: query, Logger: log}
 
 	server, _ := api.NewServer(ctx, essen)
 	server.SetRoutes()
-	server.StartServer(ctx, log, query)
+	server.StartServer(ctx)
 }
